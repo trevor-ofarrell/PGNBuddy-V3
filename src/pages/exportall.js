3f6f4ab86c52126db48df4aef68ac32539cbb052
trevor-ofarrell <trevorofarrell963@gmail.com>
2020-12-04 16:10:37 -0800

    adding sleep commands in async fucntion to insure correct execution order

--- src/pages/exportall.js
+++ src/pages/exportall.js
@@ -77,9 +77,8 @@ export const getServerSideProps = async (ctx) => {
             "user_data": {"id": props.id, "email": props.email},
         }
         await fetch('/api/exportall', {method: 'POST', body: JSON.stringify(data), headers: {'Content-Type': 'application/json'}})
-            .then(await sleep(1000))
+        await sleep(1000)
         return window.location.href = '/dashboard';
-        
     }
        
     return (

52e13389929f49efe0fc740679c691be1a189631
trevor-ofarrell <trevorofarrell963@gmail.com>
2021-01-13 01:53:12 -0800

    adding meta description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -30,7 +30,7 @@ export default function SaveApp(props) {
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
           />
-          <meta name="description" content="Hi I'm Trevor O'Farrell, I'm a full stack web developer from the Bay Area. Welcome to my website!"/>
+          <meta name="description" content="Store, view, analyze, and edit your PGN files from anywhere, on any device. Import games from lichess using the lichess.org API, or upload your own files."/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Aldrich&display=swap" rel="stylesheet"/>          
         </Head>

7ab83e5935dd62c5ebaef2935abcf8685ce14f0b
trevor-ofarrell <trevorofarrell963@gmail.com>
2021-02-26 08:14:33 -0800

    debugging issues with deletepgn in production

--- src/pages/api/deletepgn.js
+++ src/pages/api/deletepgn.js
@@ -2,46 +2,45 @@ import redis from 'redis';
 import bluebird from 'bluebird';
 
 async function deletepgn(req, res) {
+  bluebird.promisifyAll(redis.RedisClient.prototype);
+
+  const cache = redis.createClient({
+    port: process.env.LAMBDA_REDIS_PORT,
+    host: process.env.LAMBDA_REDIS_ENDPOINT,
+    password: process.env.LAMBDA_REDIS_PW,
+  });
+
   if (req.method === 'POST') {
     const { id } = req.body;
     const { folderName } = req.body;
     const { pgnId } = req.body;
-
-    bluebird.promisifyAll(redis.RedisClient.prototype);
-
-    const cache = redis.createClient({
-      port: process.env.LAMBDA_REDIS_PORT,
-      host: process.env.LAMBDA_REDIS_ENDPOINT,
-      password: process.env.LAMBDA_REDIS_PW,
-    });
 
     cache.hlen(`${id}-${folderName}`, async (error, number) => {
       if (number > 1) {
         await cache.hdelAsync(`${id}-${folderName}`, pgnId, (err, reply) => {
           if (!err) {
             if (reply === 1) {
-              console.log(`${id} is deleted`);
+              res.status(200).end();
             } else {
-              console.log(`${id} doesn't exists`);
+              res.status(200).end();
             }
           } else if (err) {
             console.log(err);
-          } else {
-            console.log('cache delete failed', err);
-            cache.quit();
-            return res.status(500).end();
+            res.status(500).end();
           }
+        }).then(() => {
           cache.quit();
+          return res.status(200).end();
         });
       } else {
         await cache.sremAsync(`${id}-folder-names`, folderName);
-        await cache.delAsync(`${id}-${folderName}`);
+        await cache.delAsync(`${id}-${folderName}`).then(() => {
+          cache.quit();
+          return res.status(200).end();
+        });
       }
-      cache.quit();
-      return res.status(200).end();
     });
   }
-  return res.status(500).end();
 }
 
 export default deletepgn;
